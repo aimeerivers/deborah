0bc567093f0977f307661a7c3970b9812a8741cb
aimee daniells <aimee@edendevelopment.co.uk>
2010-06-23 17:10:57 +0100

    I'm not sure why the final step in the OAuth process is producing an invalid signature ...

--- deborah.rb
+++ deborah.rb
@@ -12,16 +12,7 @@ get '/' do
   if authenticated?
     redirect '/contacts'
   else
-    @target = 'https://www.google.com/accounts/OAuthGetRequestToken'
-    @oauth = {
-      'oauth_consumer_key' => 'anonymous',
-      'oauth_nonce' => generate_nonce,
-      'oauth_signature_method' => 'HMAC-SHA1',
-      'oauth_timestamp' => Time.now.to_i.to_s,
-      'scope' => 'http://www-opensocial.googleusercontent.com/api/people',
-    }
-    @signature = generate_signature('POST', @target, @oauth)
-    haml :index
+    redirect '/login'
   end
 end
 
@@ -30,12 +21,22 @@ get '/contacts' do
   haml :contacts
 end
 
-post '/login' do
-  if authenticate(params[:token])
-    redirect '/contacts'
-  else
-    redirect '/'
-  end
+get '/login' do
+  oauth_token, oauth_token_secret = get_request_token
+  authorize_token(oauth_token)
+end
+
+get '/continue' do
+  @target = 'https://www.google.com/accounts/OAuthGetAccessToken'
+  @oauth = {
+    'oauth_consumer_key' => 'anonymous',
+    'oauth_token' => params['oauth_token'],
+    'oauth_verifier' => params['oauth_verifier'],
+    'oauth_timestamp' => Time.now.to_i.to_s,
+    'oauth_nonce' => generate_nonce
+  }
+  @signature = generate_signature('POST', @target, @oauth)
+  haml :index
 end
 
 get '/logout' do
@@ -61,7 +62,9 @@ def generate_nonce
 end
 
 def generate_signature(method, target, oauth)
-  Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), 'anonymous&', base_string(method, target, oauth))).chomp.gsub(/\n/,'') 
+  string = base_string(method, target, oauth)
+  puts string
+  Base64.encode64(OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), 'anonymous&', string)).chomp.gsub(/\n/,'') 
 end
 
 def base_string(method, target, oauth)
@@ -75,4 +78,28 @@ end
 
 def percent_encode(string)
   URI.escape(string, Regexp.new("[^#{URI::PATTERN::UNRESERVED}]") ).gsub('*', '%2A')
+end
+
+def get_request_token
+  target = 'https://www.google.com/accounts/OAuthGetRequestToken'
+  oauth = {
+    'oauth_consumer_key' => 'anonymous',
+    'oauth_nonce' => generate_nonce,
+    'oauth_signature_method' => 'HMAC-SHA1',
+    'oauth_timestamp' => Time.now.to_i.to_s,
+    'scope' => 'http://www-opensocial.googleusercontent.com/api/people',
+    'oauth_callback' => 'http://localhost:4567/continue'
+  }
+  signature = generate_signature('POST', target, oauth)
+  result = CGI.parse(RestClient.post(target, oauth.merge('oauth_signature' => signature)))
+  [result['oauth_token'].first, result['oauth_token_secret'].first]
+end
+
+def authorize_token(token)
+  redirect 'https://www.google.com/accounts/OAuthAuthorizeToken?oauth_token=' + token
+end
+
+def get_access_token(token, verifier)
+  result = CGI.parse(RestClient.post(target, oauth.merge('oauth_signature' => signature)))
+  raise result.inspect
 end
